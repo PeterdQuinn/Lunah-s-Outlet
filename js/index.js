6411768fe3359b31e048d6e79aac031119aaaa54
Peter Quinn <peter@quickorganics.com>
2023-03-11 13:32:41 -0700

    added mexico

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const hamburer = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
+const mexicoBtn = document.querySelector('#mexico-btn');
+
 
 if (hamburer) {
   hamburer.addEventListener("click", () => {
@@ -7,3 +9,8 @@ if (hamburer) {
   });
 }
 
+
+mexicoBtn.addEventListener('click', function() {
+  // Redirect the user to the linked website when the button is clicked
+  window.location.href = 'https://amzn.to/3yu54mY';
+});
